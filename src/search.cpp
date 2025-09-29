4c9904bc450fb5171caf2c97bb89d0a1eb0917a8
eleanororo <eleanorrojerrr@gmail.com>
2025-09-29 17:18:17 -0400

    This is so hard ahhhh

---
+++ src/search.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+
+bool searchMatrix(std::vector<std::vector<int>> matrix, int target){
+    for (int i = 0; i < matrix.size(); ++i) {
+        for (int j = 0; j< matrix[i].size();++j){
+            
+        }
+    }
+}
